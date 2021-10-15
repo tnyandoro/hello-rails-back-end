a68ae2997bf96c5e307e2e2ab5798b2ae9844efb
tnyandoro <tendai@hotmail.com>
2021-10-16 01:05:14 +0200

    Add fastjsonapi and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+messages = Message.create([
+    {
+      id: 1,
+      greeting: 'Howzit buddy'
+    },
+   {
+      id: 2,
+      greeting: 'Titambire'
+    },
+    {
+      id: 3,
+      greeting: 'Share a smile today will be a great day'
+    },
+    {
+      id: 4,
+      greeting: 'Today is great day'
+    },
+    {
+      id: 5,
+      greeting: 'You are looking great to day friend'
+    },
+    {
+      id: 6,
+      greeting: 'Hey hows your day'
+    }
+  ])
+  

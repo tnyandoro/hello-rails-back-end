9948284de143bc04bc6e9f71cb66110918a118a3
tnyandoro <tendai@hotmail.com>
2021-10-16 01:44:16 +0200

    Add the show method in the api controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -5,6 +5,11 @@ module Api
                 messages = Message.all
                 render json: MessageSerializer.new(messages).serialized_json
             end
+
+            def show
+                message = Message.find(params[:id])
+                render json: MessageSerializer.new(message).serialized_json
+            end
         end
     end
 end

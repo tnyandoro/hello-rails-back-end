4e7bce2637cdf84e428f85f17325670dd4837ccd
tnyandoro <tendai@hotmail.com>
2021-10-16 02:40:33 +0200

    Add all message methods in the api

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -10,6 +10,36 @@ module Api
                 message = Message.find(params[:id])
                 render json: MessageSerializer.new(message).serialized_json
             end
+
+            def create
+                message = Message.new(message_params)
+                if message.save
+                    render json: MessageSerializer.new(message).serialized_json
+                else
+                    render json: {errors: message.errors.full_messages}, status: 422
+                end
+            end
+
+            def update
+                message = Message.find(params[:id])
+                if message.update(message_params)
+                    render json: MessageSerializer.new(message).serialized_json
+                else
+                    render json: {errors: message.errors.full_messages}, status: 422
+                end
+            end
+
+            def destroy
+                message = Message.find(params[:id])
+                message.destroy
+                render json: {message: "Message deleted"}
+            end
+
+            private
+
+            def message_params
+                params.require(:message).permit(:greeting, :user_id)
+            end
         end
     end
 end

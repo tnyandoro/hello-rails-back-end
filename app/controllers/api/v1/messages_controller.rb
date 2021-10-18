2d241b3627dd6c04e030b4d44ee690624d6bd4e3
tnyandoro <tendai@hotmail.com>
2021-10-18 14:47:04 +0200

    Add more dependencies and delete lockfile gem

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,51 +1,24 @@
 # app/controllers/api/v1/messages_controller.rb
 module Api
   module V1
-    class MessagesController < ApplicationController
-      # protect_from_forgery with: :null_session
-
+    class Api::V1::MessagesController < ApplicationController
       def index
-        messages = Message.all
-        render json: MessageSerializer.new(messages).serialized_json
+        @messages = Message.all
+        render json: @messages
       end
 
       def show
-        message = Message.find(id: params[:id]) || Message.random
-        render json: MessageSerializer.new(message).serialized_json
+        @message = Message.find(params[:id])
+        render json: @greeting
       end
 
       def create
-        message = Message.new(message_params)
-        if message.save
-          render json: MessageSerializer.new(message).serialized_json
+        @message = Message.new(message_params)
+        if @message.save
+          render json: @message, status: :created
         else
-          render json: { error: message.errors.messages }, status: 422
+          render json: @message.errors, status: :unprocessable_entity
         end
-      end
-
-      def update
-        message = Message.find(params[:id])
-        if message.update(message_params)
-          render json: MessageSerializer.new(message).serialized_json
-        else
-          render json: { error: message.errors.messages }, status: 422
-        end
-      end
-
-      def destroy
-        message = Message.find(params[:id])
-
-        if message.destroy
-          head :no_content, status: :ok
-        else
-          render json: { error: message.errors.messages }, status: 422
-        end
-      end
-
-      private
-
-      def message_params
-        params.require(:message).permit(:greeting, :user_id)
       end
     end
   end
